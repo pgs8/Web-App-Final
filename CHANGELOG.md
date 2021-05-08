c1afdd58bf3976363f9ea9d2ef51babed9d7f0a2
pgs8 <pgs8@njit.edu>
2021-05-08 11:00:47 -0400

    Feature | Created sql table for registration info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,4 @@
 Task | Forked Paola's Project, created new Branch *Registration* | Phil | 20210508
 Task | add env file to gitignore | Phil | 20210508
 Task | updated Requirements for SendGrid | Phil | 20210508
+Feature | Created sql table for registration info | Phil | 20210508
--- db/init.sql
+++ db/init.sql
@@ -24,3 +24,14 @@ VALUES (1958, 340, 318, 362, 348, 363, 435, 491, 505, 404, 359, 310, 337),
        (1959, 360, 342, 406, 396, 420, 472, 548, 559, 463, 407, 362, 405),
        (1960, 417, 391, 419, 461, 472, 535, 622, 606, 508, 461, 390, 432);
 
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  	`username` varchar(50) NOT NULL,
+  	`password` varchar(255) NOT NULL,
+  	`email` varchar(100) NOT NULL,
+  	'confirmed' boolean DEFAULT FALSE NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`, `confirmed`) VALUES (1, 'test', 'test', 'test@test.com', TRUE);
